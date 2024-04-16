6f68d7579442203680348c897352c4b459a8a257
Palmatt98 <palmierimatteo98@gmail.com>
2024-04-16 16:39:28 +0200

    bundle-1 -2

--- js-debug/bundle-01/main.js
+++ js-debug/bundle-01/main.js
@@ -11,10 +11,12 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
-
+//1- dovrebbe stampare in console 5 volte (i)
+//2- no
+//3- si perché non verrà mai eseguita alcuna istruzione all'interno del for perché la condizione risulta sempre falsa, sin dall'inizio.
 
 // ESERCIZIO 2
 function addIfEven(num) {
@@ -23,7 +25,9 @@ function addIfEven(num) {
     }
     return num;
 }
-
+// 1- se il numero é pari dovrebbe addizionare 5 e ritornare il risultato del numero altrimenti ritorna lo stesso numero passato come parametro 
+//2- si: = ---> ===. confusione tra assegnazione(=) e uguaglianza(===)
+//3-no
 
 // ESERCIZIO 3
 function loopToFive() {
@@ -31,17 +35,25 @@ function loopToFive() {
         console.log(i);
     }
 }
-
+//1- dovrebbe stampare i numeri da 0 a 4
+//2- si: le istruzioni all'interno delle tonde del for devono essere suddivise con ";" e non da ","
+//3- no
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
+    for (let i = 0; i < numbers.length - 1; i++) {
         if (numbers % 2 = 0); {
             evenNumbers.push(i);
         }
         return evenNumbers;
     }
 }
+//1- dovrebbe stampare un array di numeri pari da 2 a 8 
+//2- si: non si deve mettere ";" dopo l'incremento "i++".
+//   non deve esserci l'assegnazione "=" ma deve esserci l'uguaglianza "==="
+//  non deve esserci (i) all'interno di push perché andrebbe a prendere solamenten il numero anzichè l'indice di numbers quindi dovrebbe essere numbers[i]
+//3-no
+
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
--- js-debug/bundle-02/main.js
+++ js-debug/bundle-02/main.js
@@ -11,62 +11,72 @@
 
 
 // ESERCIZIO 1
-function checkAge() {
-    const myAge = 32;
-    const message = '';
+// function checkAge() {
+//     const myAge = 32;
+//     const message = '';
 
-    if (myAge < 18) {
-        message = `Sei troppo giovane! Hai ${myAge} anni!`;
-    } else {
-        message = 'Hai più di 18 anni!';
-    }
-}
-checkAge();
+//     if (myAge < 18) {
+//         message = `Sei troppo giovane! Hai ${myAge} anni!`;
+//     } else {
+//         message = 'Hai più di 18 anni!';
+//     }
+// }
+
+// checkAge()
+//1- il codice dovrebbe dire se sei maggiorenne o no
+//2-no
+//3- si: non abbiamo dove stampare il messagio. L'utente non ha dove inserire un'età e la funzione è stata invocata senza alcuno scopo
 
 // ESERCIZIO 2
-function printColorsNumber() {
-    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
-}
-printColorsNumber();
+// function printColorsNumber() {
+//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+//     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+// }
+// printColorsNumber();
+//1- il codice dice quanti colori ci sono nella palette
+//2-si: length è scritto in maniera sbagliata 
+//3-no
 
 
-// ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
+// // ESERCIZIO 3
+// function addNumbers() {
+//     const userNumber = prompt('Inserisci un numero');
+//     const total = userNumber + 12;
 
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
+//1- il codice dice di addizionare 12 al numero inserito dall'utente
+//2- no
+//3- si: bisogna trasmofrmare in numero il prompt perchè altrimenti andremmo ad addizionare una stringa ad un numero e sappiamo che non si può fare
 
+// // ESERCIZIO 4
+// function checkAccess() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-// ESERCIZIO 4
-function checkAccess() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
+//     let grantAccess = 'false';
 
-    let grantAccess = 'false';
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = 'true';
+//     }
 
-    if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
-    }
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
+//1- acconsente l'accesso solo se la mail è corretta 
+//2-no
+//3-si: i valori booleani di  grantAccess della prima variabile e della prima condizione sono stati messi sottoforma dato stringa anzichè dato booleano 
 
-    if (grantAccess === true) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
-    }
-}
-checkAccess();
-
-
-// ESERCIZIO 5 (suggerimento: c'è un solo errore)
+// // ESERCIZIO 5 (suggerimento: c'è un solo errore)
 function checkAccessImproved() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
     const userEmail = prompt('Inserisci il tuo indirizzo email');
-
     let grantAccess = 'false';
 
     for (let i = 0; i < addresses.length; i++) {
@@ -87,11 +97,14 @@ function checkAccessImproved() {
             console.log('Accesso negato!');
         }
     }
+    
     checkAccessImproved();
 
+}
 
-
-
+//1-
+//2-
+//3-
 
 
 
